e641f5f4f9f304a4a9c01c72e3abad8e9fbf8f36
Shickender <bashashickender@gmail.com>
2025-02-26 17:37:33 +0530

    commit second project

---
+++ src/Usage_Github/sample.java
@@ -0,0 +1,14 @@
+package Usage_Github;
+
+public class sample {
+	public void hubb() 
+	{
+	System.out.println("new Project");
+
+	}
+public static void main(String[] args) {
+sample s=new sample();
+s.hubb();
+	
+}
+}
